40f264211baa684575dfb6fcde0f135e95ec4173
Jak Ramos <142371206+jk-ramos@users.noreply.github.com>
2025-09-13 23:10:16 -0300

    Add files via upload

---
+++ ATV2_SistemaBiblioteca-Flask-LDW_API/app.py
@@ -0,0 +1,15 @@
+# Importando o Flask
+from flask import Flask
+# Importando o controller (routes.py)
+from controllers import routes
+
+# Criando uma instância do Flask
+app = Flask(__name__, template_folder="views", static_folder="static") 
+
+# Inicializando as rotas
+routes.init_app(app)
+
+# Se for executado diretamente pelo interpretador
+if __name__ == "__main__":
+    # Iniciando o servidor
+    app.run(host="localhost", port=5000, debug=True)
